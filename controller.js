9a5477e53a78c71fc86e8430ff7308484fe3dc2a
karinaehoff <hoffka04@luther.edu>
2018-04-07 17:52:47 -0500

    Updates before Dinner, Yum

--- controller.js
+++ controller.js
@@ -30,36 +30,42 @@ function findMovie() {
   let title = document.querySelector("#movieTitle").value;
   let api_key = "270ef537760087ddcea25e06616b754d"
 
-    // Fetches movie id
+  // Fetches movie id
   let config1 = {}; // object, here's the method, body, headers
   config1.method = 'GET';
   config1.headers = {'Content-Type': 'application/json', 'Accept': 'application/json'};
-  fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${title}`, config1) //return a promise that contains details about the response
+  let movieId = fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${title}`, config1) //return a promise that contains details about the response
     .then(function(response) {
       return response.json();
     })
     .then(function(data) {
       let firstResult = data["results"][0];
-      movieId = firstResult["id"]
+      findMovieSummary(firstResult["id"]);
     });
+    let titleDisplay = document.querySelector("#displayMovieTitle")
+    titleDisplay.innerHTML = title;
+}
+
+function findMovieSummary(movieId) {
+  let api_key = "270ef537760087ddcea25e06616b754d"
 
   // Fetch Movie Information
-  let movieId = 505;
   let config2 = {}; // object, here's the method, body, headers
   config2.method = 'GET';
   config2.headers = {'Content-Type': 'application/json', 'Accept': 'application/json'};
-  fetch(`https://api.themoviedb.org/3/search/movie/${movieId}?api_key=${api_key}`, config2) //return a promise that contains details about the response
+  fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${api_key}`, config2) //return a promise that contains details about the response
     .then(function(response) {
       return response.json();
     })
     .then(function(data) {
-      console.log(data)
-      // let firstResult = data["results"][0];
-      // let movieId = firstResult["id"]
-      // document.querySelector("#displayMovieSummary").innerHTML = movieId;
-      let summary = "Display Summary Here";
-      document.querySelector("#displayMovieSummary").innerHTML = summary;
+      console.log(data);
+
+      //add tagline?
+
+      let overview = data["overview"];
+      document.querySelector("#displayMovieSummary").innerHTML = overview;
+
+      let poster_path = data["poster_path"];
+      document.querySelector("#displayMoviePoster").src = poster_path;
     });
-    let titleDisplay = document.querySelector("#displayMovieTitle")
-    titleDisplay.innerHTML = title;
 }
