31ac25baf326f87d840e7313532710bac4e1e2de
khummel <hummka01@luther.edu>
2018-04-08 22:53:09 -0500

    title, author, rating, summary, picture added

--- controller.js
+++ controller.js
@@ -4,8 +4,8 @@
 //Date: 3 April 2018
 
 function findBook() {
-  let title = document.querySelector("#bookTitle").value;
-  let author = document.querySelector("#author").value;
+  let title = document.querySelector("#bookTitleInput").value;
+  let author = document.querySelector("#authorInput").value;
   let config = {}; // object, here's the method, body, headers
   config.method = 'GET';
   config.headers = {'Content-Type': 'application/json', 'Accept': 'application/json'};
@@ -15,14 +15,35 @@ function findBook() {
       return response.json();
     })
     .then(function(data) {
-      let x = data["items"][0]["volumeInfo"]["description"];
-      
-      let displayDiv = document.getElementById("findBookOutput")
-      let titleDisplay = document.getElementById("bookTitle")
+      let title = data["items"][0]["volumeInfo"]["title"];
+      let author = data["items"][0]["volumeInfo"]["authors"][0];
+      let summary = data["items"][0]["volumeInfo"]["description"];
+      let avgRating = data["items"][0]["volumeInfo"]["averageRating"];
+      let bookImage = data["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"];
+
+      let titleDisplay = document.getElementById("bookTitle");
+      let authorDisplay = document.getElementById("bookAuthor");
+      let summaryDisplay = document.getElementById("bookSummary");
+      let avgRatingDisplay = document.getElementById("avgRating");
+      let possWrongResultDisplay = document.getElementById("possWrongResult");
+      let bookImageDisplay = document.getElementById("bookImage");
+
       titleDisplay.innerHTML = title;
-      let summaryDisplay = document.getElementById("bookSummary")
-      summaryDisplay.innerHTML = x;
-      console.log(displayDiv)
+      authorDisplay.innerHTML = author;
+      possWrongResultDisplay.innerHTML = "Not what you're looking for? Please check your spelling and be sure to input the full title and author's name."
+      bookImageDisplay.innerHTML =  `<img src=${bookImage} style=width:167px;height:270px>`;
+;
+
+      if (summary == undefined) {
+        summaryDisplay.innerHTML = "Summary unavailable";
+      } else {
+        summaryDisplay.innerHTML = summary;
+      }
+      if (avgRating == undefined) {
+        avgRatingDisplay.innerHTML = "Not rated";
+      } else {
+        avgRatingDisplay.innerHTML = `Average rating: ${avgRating}/5`;
+      }
     });
 }
 
